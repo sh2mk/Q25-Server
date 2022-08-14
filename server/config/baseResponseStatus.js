3528281415f81f21dc339cf4cc8a422bb77ec7d8
sh2mk <90611872+sh2mk@users.noreply.github.com>
2022-08-14 23:58:15 +0900

    커밋2
    커밋

--- server/config/baseResponseStatus.js
+++ server/config/baseResponseStatus.js
@@ -44,7 +44,7 @@ module.exports = {
     POST_CONTENT_LENGTH : { "isSuccess": false, "code": 2024, "message": "게시물 내용은 450자 미만으로 입력해주세요."},
     POST_STATUS_INACTIVE : {"isSuccess" : false, "code": 2025, "message" : "이미 삭제된 게시물입니다. "},
 
-    //비밀번호 변경
+    //비밀번호 변경관련
     UPDATE_PW_WRONG : {"isSuccess":false, "code" : 2026, "message": "비밀번호를 정확하게 입력해주세요." },
 
     // Response error(의미적 validation error)
