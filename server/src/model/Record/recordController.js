4154cb76d6b0ea41116801b1491482f57af1163f
sh2mk <90611872+sh2mk@users.noreply.github.com>
2022-08-15 00:19:51 +0900

    ㅁㅁ

--- server/src/model/Record/recordController.js
+++ server/src/model/Record/recordController.js
@@ -60,7 +60,8 @@ exports.getCollection = async function (req,res) {
 //질문리스트 -------------------------------------------------------------쿼리 못받아서 userIdx 강제입력
 exports.getQlist = async function (req,res) {
     /*
-        body : userIdx, questionIdx
+        body : 
+        쿼리 : userIdx
     */
    const { userIdx } = req.body;
    //사용할 userIdx 번호 넣어서 하시면 됩니다 
